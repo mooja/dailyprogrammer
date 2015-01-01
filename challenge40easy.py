13df25d7aaf994b28a85405d67d110176343560f
Max Shkurygin <max.atreides@gmail.com>
2015-01-01 08:16:20 -0500

    challenge 40 easy

---
+++ challenge40easy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+#######################################################################
+# challenge 40 easy                                                   #
+# print numbers 1 to 1000 without any loop or conditional statements  #
+#######################################################################
+
+
+if __name__ == '__main__':
+    print '\n'.join(map(str, xrange(1, 1001)))
