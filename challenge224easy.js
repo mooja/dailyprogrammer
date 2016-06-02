02b7b285565ac5ca6be9af8669b20d76e6dcd737
Max Shkurygin <max.atreides@gmail.com>
2016-06-02 09:31:37 -0400

    re-write

--- challenge224easy.js
+++ challenge224easy.js
@@ -8,27 +8,42 @@
 
 // random using current time + linear congruent generator
 // weak + breaks on big numbers, don't use in poker
-function rand_range(lo, hi) {
-    let now = (new Date().getTime());
-    let modulus = hi - lo;
-    let pseudo_random_num = (5 * now + 2) % modulus;
-    return lo + pseudo_random_num;
+
+function lcg_factory(
+    seed, 
+    modulus=Math.pow(2, 24), // returns random 3 bytes
+    a=1140671485, // taken from visual basic
+    c=2531011)    // taken from visual basic
+{
+    let state = seed;
+    function lgc() {
+        state = (state*a + c) % modulus;
+        return state
+    }
+    return lgc;
 }
 
-Array.prototype.swap = function(i, j) {
+Array.prototype.swap = function(i, j) 
+{
     let temp = this[i];
     this[i] = this[j];
     this[j] = temp;
 }
 
 // Fisher Yates Shuffle
-Array.prototype.shuffle = function() {
+Array.prototype.shuffle = function() 
+{
+    let rand = lcg_factory((new Date().getTime()));
     for(let i = this.length-1; i > 0; i--) {
-        let r = rand_range(0, i);
+        let r = rand() % i;
         this.swap(i, r)
     }
     return this;
 }
 
-var set = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(set.shuffle())
+let set = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(set.shuffle());
+
+let rand_range = lcg_factory((new Date().getTime()));
+for(let i=0; i < 10; i++)
+    console.log(rand_range())
