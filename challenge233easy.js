cfcc3cc7510c6641d1b686abb9db133109a935d9
Max Shkurygin <max.atreides@gmail.com>
2016-06-11 10:07:17 -0400

    challenge 233 easy

--- challenge233easy.js
+++ challenge233easy.js
@@ -11,150 +11,157 @@ _ = require('lodash');
 const WIDTH_UNIT_CHARS = 5; 
 const HEIGHT_UNIT_CHARS=3;
 
-const raw_input = 
+const raw_input1 = 
 `
    *
   ***
 ******
 `
 
-function get_column_heights( input ) {
-  let lines = input.split('\n');
-  let line_lengths = lines.map(l => l.length);
-  let longest_line = _.max(line_lengths);
+const raw_input2 = 
+`
+ *
+***
+***
+***
+***
+***
+***
+`
 
-  // build up a columns array-like object. 
-  // column[0] = size of the first column, etc
-  let columns = {}
-  for (let line of lines) {
-    for(let col_idx = 0; col_idx < line.length; col_idx++) {
-      if (line[col_idx] === '*')
-        columns[col_idx] = (columns[col_idx] || 0) + 1
-    }
-  }
+const raw_input3 = 
+`
+***                    ***
+***     **  ***  **    ***
+***   ***************  ***
+***   ***************  ***
+***   ***************  ***
+**************************
+**************************
+`
 
-  // convert culumns object to a Array object so that we have a .length
-  // property
-  let column_heights = [];
-  for (let i = 0; i < longest_line; i++) {
-    column_heights.push(columns[i.toString()]);
-  }
+class Building {
 
-  return column_heights;
-}
+  constructor (blueprint) {
+    // build up a columns array-like object. 
+    // column[0] = size of the first column, etc
+    let columns = {}
+    let lines = blueprint.split('\n');
+    for (let line of lines) 
+      for(let col_idx = 0; col_idx < line.length; col_idx++) 
+        if (line[col_idx] === '*')
+          columns[col_idx] = (columns[col_idx] || 0) + 1;
 
-function get_boxes_from_colheights( column_heights ) {
-  let boxes = [];
-  let prev_height = -1;
-  for ( let column_height of column_heights ) {
-    let box = {}
-    if (column_height === prev_height) {
-      box = boxes.pop() 
-      box.width += 1;
-    } else {
-      box['width'] = 1;
-      box['height'] = column_height;
-      prev_height = column_height;
-    }
-    boxes.push(box);
-  }
-  return boxes;
-}
+    let longest_line = _.max(lines.map(l => l.length))
+    let column_heights = [];
+    for (let i = 0; i < longest_line; i++)
+      column_heights.push(columns[i.toString()]);
 
-function draw_box(box) {
-  let height_chars = box.height * HEIGHT_UNIT_CHARS;
-  let width_chars = box.width * WIDTH_UNIT_CHARS;
-
-  let box_lines = [];
-  for(let row of _.range(height_chars)) {
-    let line = [];
-
-    for(let col of _.range(width_chars)) {
-      let is_horizontal_wall = (row === 0 || row === height_chars-1)
-      let is_vertical_wall = (col === 0 || col === width_chars-1)
-      let is_corner = is_horizontal_wall && is_vertical_wall;
-
-      if (is_corner)
-        line.push('+');
-      else if (is_vertical_wall)
-        line.push('|');
-      else if (is_horizontal_wall)
-        line.push('-');
-      else
-        line.push(' ');
-
-    } // end for cols
-
-    box_lines.push(line.join(''));
-  } // end for rows
-
-  return box_lines.join('\n');
-}
-
-// "image" = a string of a drawn box
-function combine_box_images(img1, img2) {
-  let img_height = img => img.split('\n').length;
-  let img_width = img => img.split('\n')[0].length;
-
-  // construct a two-dimentional matrix
-  let combined_width = img_width(img1) + img_width(img2);
-  let combined_height = _.max([img_height(img1), img_height(img2)]);
-  let combined_grid = [];
-  for(let row of _.range(combined_height)) {
-    combined_grid.push(' '.repeat(combined_width).split(''))
-  }
-
-  // add first image to the grid
-  let img1lines = img1.split('\n');
-  for (let row of _.range(img_height(img1))) {
-    for (let col of _.range(img_width(img1))) {
-      combined_grid[row][col] = img1lines[row][col];
-    }
-  }
-
-  // add second image to the grid
-  let img2lines = img2.split('\n');
-  let img2offset = {row: 0, 
-                    col: img_width(img1) - 1}
-
-  for (let row of _.range(img_height(img2))) {
-    for (let col of _.range(img_width(img2))) {
-      let grid_row = row + img2offset.row;
-      let grid_col = col + img2offset.col
-
-      let img2_ch = img2lines[row][col];
-      let grid_ch = combined_grid[grid_row][grid_col];
-
-      let is_overlapping_corner = (img2_ch === '+' || grid_ch === '+');
-      let is_overlapping_wall = (img2_ch === '|' && grid_ch === '|');
-
-      if (is_overlapping_corner) {
-        combined_grid[grid_row][grid_col] = '+';
-      } else if (is_overlapping_wall) {
-        combined_grid[grid_row][grid_col] = ' ';
+    let boxes = [];
+    let prev_height = -1;
+    let pos = 0; // position on the x axis
+    for ( let column_height of column_heights ) {
+      let box = {}
+      if (column_height === prev_height) {
+        box = boxes.pop() 
+        box['unit_width'] += 1;
       } else {
-        combined_grid[grid_row][grid_col] = img2_ch;
+        box['unit_width'] = 1;
+        box['unit_height'] = column_height;
+        prev_height = column_height;
       }
+
+      box['width'] = box['unit_width'] * WIDTH_UNIT_CHARS;
+      box['height'] = box['unit_height'] * HEIGHT_UNIT_CHARS;
+      box['roof_height'] = box['height'] + Math.floor((box['width'] - 3) / 2) + 1;
+      
+      boxes.push(box);
+    } // end create boxes
+
+    this.boxes = boxes;
+  } // end constructor
+
+  toString() {
+    let nboxes = this.boxes.length;
+    let grid_width = _.sum(this.boxes.map(b => b.width)) - nboxes + 1;
+    let grid_height = _.max(this.boxes.map(b => b.roof_height));
+    let grid = _.range(grid_height)
+      .map(() => 
+        ' '.repeat(grid_width)
+           .split(''));
+
+    // draw  boxes ----------------------
+    let box_pos = 0;
+    for (let box of this.boxes) {
+      for (let y of _.range(box.height)) {
+        for (let x of _.range(box.width)) {
+          let is_horizontal_wall = (y === 0 || y === box.height - 1);
+          let is_vertical_wall = (x === 0 || x === box.width - 1);
+          let is_corner = is_horizontal_wall && is_vertical_wall;
+
+          let grid_ch = grid[y][x+box_pos];
+
+          if (is_corner && grid_ch === '+')
+            grid[y][x+box_pos] = '-';
+          else if (is_corner)
+            grid[y][x+box_pos] = '+';
+          else if (is_vertical_wall && grid_ch === '+')
+            grid[y][x+box_pos] = '+';
+          else if (is_vertical_wall && grid_ch === '|')
+            grid[y][x+box_pos] = ' ';
+          else if (is_vertical_wall)
+            grid[y][x+box_pos] = '|';
+          else if (is_horizontal_wall)
+            grid[y][x+box_pos] = '-';
+
+        }
+      } // end for (let y of _.range(box.height))
+
+      box_pos += box.width - 1
+    } // end for (let box of this.boxes)
+
+
+    // draw roofs ----------------------
+    box_pos = 0;
+    for (let box of this.boxes) {
+      let x = box_pos + 1; 
+      let y = box.height;
+      let roof_side_length = Math.floor((box.width - 3) / 2)
+
+      // left side
+      for(let i = 0; i < roof_side_length; i++) {
+        grid[y][x] = '/';
+        x++;
+        y++;
+      }
+
+      // top
+      grid[y][x] = 'A';
+      x++;
+      y--;
+
+      // right side
+      for(let i = 0; i < roof_side_length; i++) {
+        grid[y][x] = '\\';
+        x++;
+        y--;
+      }
+
+      box_pos += box.width - 1;
     }
+
+    return grid.map(chars => chars.join('')).reverse().join('\n');
   }
 
-  let combined_grid_lines = combined_grid.map(arr => arr.join(''));
-  // combined_grid_lines.reverse();
-  return combined_grid_lines.join('\n');
-
-} // end combine_box_images
+}
 
 // main --
 
-let col_heights  = get_column_heights(raw_input);
-console.log(col_heights);
+let b1 = new Building(raw_input1);
+console.log(b1.toString());
 
-let boxes = get_boxes_from_colheights(col_heights);
-console.log(boxes);
+let b2 = new Building(raw_input2);
+console.log(b2.toString());
 
-let drawn_boxes = boxes.map(draw_box);
-for (let drawn_box of drawn_boxes) 
-  console.log(drawn_box);
-
-let combined_boxes = drawn_boxes.reduce(combine_box_images);
-console.log(combined_boxes);
+let b3 = new Building(raw_input3);
+console.log(b3.toString());
