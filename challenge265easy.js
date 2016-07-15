d5bd5122142aa14dd46fa7038d352ba0c78f3aa9
Max Shkurygin <max.atreides@gmail.com>
2016-07-15 17:36:10 -0400

    added combinations implementation

--- challenge265easy.js
+++ challenge265easy.js
@@ -8,6 +8,15 @@
 
 "use strict;"
 
+const _ = require("lodash");
+
+fact = _.memoize(_fact);
+function _fact(n) {
+  if (n === 1)
+    return 1;
+  return n * _fact(n-1);
+}
+
 function* permutations(seq) {
   if (seq.length === 1)
     yield seq;
@@ -28,10 +37,32 @@ function nthPermutation(n, seq) {
   }
 }
 
-// TODO
-function* combinations(seq) {
+function* combinations(seq, r) {
+  let seen = [];
+  for (let perm of permutations(seq)) {
+    let combination_candidate = perm.slice(0, r).sort();
+    let has_been_seen = false;
+    for (let seen_comb of seen)
+      if (_.isEqual(combination_candidate, seen_comb))
+        has_been_seen = true;
+    if (has_been_seen)
+      continue;
+    seen.push(combination_candidate);
+    yield combination_candidate;
+  }
 }
 
-console.log(nthPermutation(3, [0, 1, 2]));
-console.log(nthPermutation(240, [0, 1, 2, 3, 4, 5]));
+function nthCombination(n, seq, r) {
+  let counter = 0;
+  for (let comb of combinations(seq, r)) {
+    if (counter === n-1)
+      return comb;
+    counter++;
+  }
+}
+
+console.log(nthPermutation(3,    [0, 1, 2]));
+console.log(nthPermutation(240,  [0, 1, 2, 3, 4, 5]));
 console.log(nthPermutation(3240, [0, 1, 2, 3, 4, 5, 6]));
+console.log(nthCombination(24,   [0, 1, 2, 3, 4, 5, 6, 7], 3));
+console.log(nthCombination(112,  [0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
